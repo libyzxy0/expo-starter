d9fc03057a2940ed3816309c0c2bb7ce795534ae
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2025-11-22 17:27:30 +0800

    feat: inverting theme colors

--- hooks/useColors.ts
+++ hooks/useColors.ts
@@ -1,15 +1,12 @@
 import Colors from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 
-type ThemeType = "dark" | "light";
-
-export function useColors(customTheme?: ThemeType) {
-    const colorScheme = useColorScheme() ?? "light";
-
-    if (!Colors) {
-        console.error("Cannot find theme colors!");
-        return null;
-    }
-
-    return Colors[customTheme ? customTheme : colorScheme];
+export function useColors(inverted?: boolean) {
+    return Colors[
+        inverted
+            ? useColorScheme() === "dark"
+                ? "light"
+                : "dark"
+            : useColorScheme() ?? "light"
+    ];
 }
